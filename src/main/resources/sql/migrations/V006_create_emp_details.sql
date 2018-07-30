a449e232bbd69ac91f0796b351b7d2d255bb86d2
bosesu01 <sushant.bose.1984@gmail.com>
2018-07-30 16:44:35 -0400

    Update V006_create_emp_details.sql

--- src/main/resources/sql/migrations/V006_create_emp_details.sql
+++ src/main/resources/sql/migrations/V006_create_emp_details.sql
@@ -1,1 +1,10 @@
+create table address (
+  house_number      int not null,
+  street_name       varchar(20),
+  zipcode           char(7),
+  city_name         varchar(20),
+  province          varchar(20)
+);
 
+insert into address (house_number,street_name,zipcode,city_name,province) values (155, 'Wellesley St E', 'M4Y 1J4', 'Toronto', 'Ontario');
+insert into address (house_number,street_name,zipcode,city_name,province) values (100, 'Wellington St E', 'M5L 1Y4', 'Toronto', 'Ontario');
